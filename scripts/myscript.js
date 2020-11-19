491a4be563a0af1bb5e2903c6541639c9263f6ca
sazzad024 <sazzadpower7@gmail.com>
2020-11-19 23:04:00 +0600

    home page all

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -57,3 +57,11 @@ let trans = () => {
 */
 
 // score settings 
+// masonry grid settings
+$('.grid').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-sizer',
+    percentPosition: true
+  });
